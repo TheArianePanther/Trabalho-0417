7f7c1b41c8bda062a23c2abffc1c28bae225b428
TheArianePanther <137220835+TheArianePanther@users.noreply.github.com>
2024-04-17 16:46:54 -0300

    Add files via upload

---
+++ Trabalho 17042024/scriptari2.js
@@ -0,0 +1,12 @@
+        function exibirMensagem(categoria) {
+            alert('Você clicou em ' + categoria);
+            
+        }
+
+        function mudarCor(elemento) {
+            elemento.style.backgroundColor = '#bbb'; 
+        }
+
+        function restaurarCor(elemento) {
+            elemento.style.backgroundColor = ''; 
+        }
